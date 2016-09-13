fe4aac8bb196df19f2f31593e07152df48376a09
kasongoyo <kasongoyo@gmail.com>
2016-09-13 11:54:45 +0300

    fix position of clear btn when search tools disabled

--- dist/autocomplete_template_cache.js
+++ dist/autocomplete_template_cache.js
@@ -5,7 +5,7 @@ angular.module('autocomplete').run(['$templateCache', function($templateCache) {
     "<div class=\"autocomplete\">\n" +
     "    <div class=\"autocomplete__searchBox {{ attrs.inputclass }}\" ng-class=\"{'autocomplete__searchBox--focus':focusSearchBox}\">\n" +
     "        <input type=\"text\" class=\" autocomplete__inputField {{ attrs.inputclass }}\" ng-model=\"searchParam\" placeholder=\"{{ attrs.placeholder }}\" tabindex=\"{{ attrs.tabindex }}\" id=\"{{ attrs.inputid }}\" name=\"{{ attrs.name }}\" ng-required=\"{{ autocompleteRequired }}\" ng-focus=\"focusSearchBox = true; showSearchTools = false\" ng-blur=\"focusSearchBox = false\" show-focus=\"focusSearchBox\" />\n" +
-    "        <span class=\"autocomplete__clearBtn\" ng-show=\"searchParam\" title=\"Clear\" ng-click=\"focusSearchBox = true; searchParam=undefined\">&#10005</span>\n" +
+    "        <span class=\"autocomplete__clearBtn\" ng-show=\"searchParam\" title=\"Clear\" ng-click=\"focusSearchBox = true; searchParam=undefined\" ng-class=\"{'autocomplete__clearBtn--searchToolsDisable':!enableSearchTool}\">&#10005</span>\n" +
     "        <span class=\"autocomplete__searchToolsBtnIcon\" title=\"Search options\" ng-click=\"showSearchTools = !showSearchTools; completing=false\" ng-show=\"enableSearchTool\">&#9662</span>\n" +
     "    </div>\n" +
     "    <div class=\"autocomplete__suggestions\" ng-if=\"!showSearchTools\">\n" +
